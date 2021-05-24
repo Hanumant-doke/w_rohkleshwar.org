859b3c685ff977a1ee6c4b5c20c3098d945e44ca
Hanumant-doke <hanumantdoke143@gmail.com>
2021-05-24 13:08:56 +0530

    heading

---
+++ src/PortalDemo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'  
+import ReactDOM from 'react-dom'  
+  
+class PortalDemo extends Component{  
+  render(){
+    return(  
+      <h1>Portals Demo</h1>
+     
+    );
+  }
+  }  
+export default PortalDemo;
--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react';  
+import ReactDOM from 'react-dom';  
 import { Route, BrowserRouter as Router } from 'react-router-dom'  
-import About from './About'
-import Contact from './Contact';
-import student from './student';
-import satish from './satish';
-import demo1 from './demo1';
-import demo2 from './demo2'
+import './index.css';  
+import App from './App'; 
+// import 'bootstrap/dist/css/bootstrap.css';
+// import $ from 'jquery';  
+// import Popper from 'popper.js';  
+// import 'bootstrap/dist/js/bootstrap.bundle.min';  
+import abc from './abc';
+import About from './About';
+
 const routing = (  
-    <Router>  
-      <div>  
-        <Route exact path="/" component={App} />  
-        <Route exact path="/about" component={About} />  
-        <Route exact path="/contact" component={Contact} />  
-        <Route exact path="/student" component={student} />  
-        <Route exact path="/satish" component={satish} /> 
-        <Route exact path="/demo1" component={demo1} /> 
-        <Route exact path="/demo2" component={demo2} />
-      </div>  
-    </Router>  
-  )  
-  ReactDOM.render(routing, document.getElementById('root'));  
+  <Router>  
+    <div> 
+      <Route  path="/App" component={App} />  
+       <Route exact path="/abc" component={abc} />
+       <Route exact path="/about" component={About} />
+    </div>  
+  </Router>  
+)  
+ReactDOM.render(routing, document.getElementById('root'));  
